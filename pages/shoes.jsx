7d9eab243a2fe9d3131fcb55e93df0f875016935
dhungwt <112122672+dhungwt@users.noreply.github.com>
2023-06-28 10:43:39 -0400

    added pkgs, routes and installed architecture

---
+++ pages/shoes.jsx
@@ -0,0 +1,13 @@
+//list all shoes
+
+import React from 'react'
+
+const shoes = () =>{
+    return (
+    <div>
+    <h1>Shoes Page</h1>
+    <ListItems/>
+   </div>)
+}
+
+export default shoes;
---
+++ pages/shoppers.jsx
@@ -0,0 +1,14 @@
+//list all shoppers
+import React from 'react'
+import ListItems from '../frontendproj/src/components/listItems';
+
+const shoppers = () =>{
+    return (
+    <div>
+    <h1>Shoppers Page</h1>
+    <ListItems/>
+    </div>
+    )
+}
+
+export default shoppers;
